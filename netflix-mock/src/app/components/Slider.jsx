754c3902b370d72f03a45af1d9b5e87b650b6a2b
Eric Larwa <ericlarwa@erics-air.intra.ecu.edu>
2024-01-29 14:53:47 -0500

    added section component

---
+++ netflix-mock/src/app/components/Slider.jsx
@@ -0,0 +1,30 @@
+import React from "react"; 
+import Slider from 'react-slick';
+
+const Section = ({title, series}) => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1
+
+    }
+
+    return (
+        <div className="section">
+            <h2 className="title">{title}</h2>
+            <Slider {...settings}>
+                {series.map((serie) => (
+                    <div key = {serie.id} className="serie">
+                        <img src={serie.image} alt={serie.title} />
+                        <p>{serie.title}</p>
+                    </div>
+                ))}
+            </Slider>
+        </div>
+    )
+
+}
+
+export default Section;
